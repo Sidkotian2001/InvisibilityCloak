27d0dc3b26de95d5a37c8b7e0538cccf5b0502d6
Siddharth Kotian <siddharthkotian3112@gmail.com>
2022-09-09 15:01:04 +0530

    functional changes

--- findcolorspace.py
+++ findcolorspace.py
@@ -1,88 +1,96 @@
 #finding hsv range of target object(pen)
-import cv2
+import cv2 as cv
 import numpy as np
 import time
 # A required callback method that goes into the trackbar function.
 def nothing(x):
     pass
 
-# Initializing the webcam feed.
-cap = cv2.VideoCapture(0)
-cap.set(3,1280)
-cap.set(4,720)
+def colour_tracker(cap):
+    # Initializing the webcam feed.
+    cap.set(3,1280)
+    cap.set(4,720)
 
-# Create a window named trackbars.
-cv2.namedWindow("Trackbars")
+    # Create a window named trackbars.
+    cv.namedWindow("Trackbars")
 
-# Now create 6 trackbars that will control the lower and upper range of 
-# H,S and V channels. The Arguments are like this: Name of trackbar, 
-# window name, range,callback function. For Hue the range is 0-179 and
-# for S,V its 0-255.
-cv2.createTrackbar("L - H", "Trackbars", 0, 179, nothing)
-cv2.createTrackbar("L - S", "Trackbars", 0, 255, nothing)
-cv2.createTrackbar("L - V", "Trackbars", 0, 255, nothing)
-cv2.createTrackbar("U - H", "Trackbars", 179, 179, nothing)
-cv2.createTrackbar("U - S", "Trackbars", 255, 255, nothing)
-cv2.createTrackbar("U - V", "Trackbars", 255, 255, nothing)
- 
-while True:
+    # Now create 6 trackbars that will control the lower and upper range of 
+    # H,S and V channels. The Arguments are like this: Name of trackbar, 
+    # window name, range,callback function. For Hue the range is 0-179 and
+    # for S,V its 0-255.
+    cv.createTrackbar("L - H", "Trackbars", 0, 179, nothing)
+    cv.createTrackbar("L - S", "Trackbars", 0, 255, nothing)
+    cv.createTrackbar("L - V", "Trackbars", 0, 255, nothing)
+    cv.createTrackbar("U - H", "Trackbars", 179, 179, nothing)
+    cv.createTrackbar("U - S", "Trackbars", 255, 255, nothing)
+    cv.createTrackbar("U - V", "Trackbars", 255, 255, nothing)
     
-    # Start reading the webcam feed frame by frame.
-    ret, frame = cap.read()
-    if not ret:
-        break
-    # Flip the frame horizontally (Not required)
-    frame = cv2.flip( frame, 1 ) 
-    
-    # Convert the BGR image to HSV image.
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    
-    # Get the new values of the trackbar in real time as the user changes 
-    # them
-    l_h = cv2.getTrackbarPos("L - H", "Trackbars")
-    l_s = cv2.getTrackbarPos("L - S", "Trackbars")
-    l_v = cv2.getTrackbarPos("L - V", "Trackbars")
-    u_h = cv2.getTrackbarPos("U - H", "Trackbars")
-    u_s = cv2.getTrackbarPos("U - S", "Trackbars")
-    u_v = cv2.getTrackbarPos("U - V", "Trackbars")
- 
-    # Set the lower and upper HSV range according to the value selected
-    # by the trackbar
-    lower_range = np.array([l_h, l_s, l_v])
-    upper_range = np.array([u_h, u_s, u_v])
-    
-    # Filter the image and get the binary mask, where white represents 
-    # your target color
-    mask = cv2.inRange(hsv, lower_range, upper_range)
- 
-    # You can also visualize the real part of the target color (Optional)
-    res = cv2.bitwise_and(frame, frame, mask=mask)
-    
-    # Converting the binary mask to 3 channel image, this is just so 
-    # we can stack it with the others
-    mask_3 = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-    
-    # stack the mask, orginal frame and the filtered result
-    stacked = np.hstack((mask_3,frame,res))
-    
-    # Show this stacked frame at 40% of the size.
-    cv2.imshow('Trackbars',cv2.resize(stacked,None,fx=0.4,fy=0.4))
-    
-    # If the user presses ESC then exit the program
-    key = cv2.waitKey(1)
-    if key == 27:
-        break
-    
-    # If the user presses `s` then print this array.
-    if key == ord('s'):
+    while True:
         
-        thearray = [[l_h,l_s,l_v],[u_h, u_s, u_v]]
-        print(thearray)
+        # Start reading the webcam feed frame by frame.
+        ret, frame = cap.read()
+        if not ret:
+            break
+        # Flip the frame horizontally (Not required)
+        frame = cv.flip( frame, 1 ) 
         
-        # Also save this array as penval.npy
-        np.save('hsv_value',thearray)
-        break
+        # Convert the BGR image to HSV image.
+        hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+        
+        # Get the new values of the trackbar in real time as the user changes 
+        # them
+        l_h = cv.getTrackbarPos("L - H", "Trackbars")
+        l_s = cv.getTrackbarPos("L - S", "Trackbars")
+        l_v = cv.getTrackbarPos("L - V", "Trackbars")
+        u_h = cv.getTrackbarPos("U - H", "Trackbars")
+        u_s = cv.getTrackbarPos("U - S", "Trackbars")
+        u_v = cv.getTrackbarPos("U - V", "Trackbars")
     
-# Release the camera & destroy the windows.    
-cap.release()
-cv2.destroyAllWindows()
+        # Set the lower and upper HSV range according to the value selected
+        # by the trackbar
+        lower_range = np.array([l_h, l_s, l_v])
+        upper_range = np.array([u_h, u_s, u_v])
+        
+        # Filter the image and get the binary mask, where white represents 
+        # your target color
+        mask = cv.inRange(hsv, lower_range, upper_range)
+    
+        # You can also visualize the real part of the target color (Optional)
+        res = cv.bitwise_and(frame, frame, mask=mask)
+        
+        # Converting the binary mask to 3 channel image, this is just so 
+        # we can stack it with the others
+        mask_3 = cv.cvtColor(mask, cv.COLOR_GRAY2BGR)
+        
+        # stack the mask, orginal frame and the filtered result
+        stacked = np.hstack((mask_3,frame,res))
+        
+        # Show this stacked frame at 40% of the size.
+        cv.imshow('Trackbars',cv.resize(stacked,None,fx=0.4,fy=0.4))
+        
+        # If the user presses ESC then exit the program
+        key = cv.waitKey(1)
+        
+        if key & 0xFF == ord('q'):
+            break
+        
+        # If the user presses `s` then print this array.
+        if key == ord('s'):
+            
+            thearray = [[l_h,l_s,l_v],[u_h, u_s, u_v]]
+            print(thearray)
+            
+            # Also save this array as penval.npy
+            np.save('hsv_value',thearray)
+            break
+ 
+
+
+def main():
+    cap = cv.VideoCapture(0)
+    colour_tracker(cap)
+    cap.release()
+    cv.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
--- invisibility_cloak.py
+++ invisibility_cloak.py
@@ -1,62 +1,83 @@
 import cv2 as cv 
 import numpy as np
 import sys
+import time
+import findcolorspace
 
-cap = cv.VideoCapture(0)
-frame_num = 0
-lower_hue = int(sys.argv[1])
-lower_sat = int(sys.argv[2])
-lower_val = int(sys.argv[3])
-upper_hue = int(sys.argv[4])
-upper_sat = int(sys.argv[5])
-upper_val = int(sys.argv[6])
-while cap.isOpened():
+def colour_extract():
+    findcolorspace.main()
 
-    if frame_num == 0:
+def invisible(cap):
+    #Load from numpy file
+    arr = np.load('hsv_value.npy')
+
+    #HSV values
+    lower_hue = arr[0][0]
+    lower_sat = arr[0][1]
+    lower_val = arr[0][2]
+    upper_hue = arr[1][0]
+    upper_sat = arr[1][1]
+    upper_val = arr[1][2]
+
+    end_time = time.time() + 2
+    original_image = None
+
+    #Capturing the background
+    print("Started Background capture")
+    
+    while(time.time() < end_time):
         ret, original_frame = cap.read()
-        original_image = cv.flip(original_frame, 1)
-        cv.waitKey(5000)
+        original_image = cv.flip(original_frame,1)
+    
+    print("Finished Capturing Background")
+    while cap.isOpened():
+            
+        ret, frame = cap.read()
+        image = cv.flip(frame, 1)
 
-    ret, frame = cap.read()
-    image = cv.flip(frame, 1)
+        img_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
-    img_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+        img_hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+        ret, thresh = cv.threshold(img_gray, 127, 255, cv.THRESH_BINARY)
 
-    img_hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
-    ret, thresh = cv.threshold(img_gray, 127, 255, cv.THRESH_BINARY)
+        lower_colour_value = np.array([lower_hue, lower_sat, lower_val])
+        upper_colour_value = np.array([upper_hue, upper_sat, upper_val])
 
-    lower_colour_value = np.array([lower_hue, lower_sat, lower_val])
-    upper_colour_value = np.array([upper_hue, upper_sat, upper_val])
+        mask = cv.inRange(img_hsv, lower_colour_value, upper_colour_value)
 
-    mask = cv.inRange(img_hsv, lower_colour_value, upper_colour_value)
+        res = cv.bitwise_and(image, image, mask = mask)
 
-    res = cv.bitwise_and(image, image, mask = mask)
+        erode = cv.erode(mask, None, iterations = 2)
+        dilate = cv.dilate(erode, None, iterations = 2)
 
-    erode = cv.erode(mask, None, iterations = 2)
-    dilate = cv.dilate(erode, None, iterations = 2)
+        guassian_blur = cv.GaussianBlur(dilate,(3,3),0)
 
-    guassian_blur = cv.GaussianBlur(dilate,(3,3),0)
+        median = cv.medianBlur(guassian_blur, 5)
 
-    median = cv.medianBlur(guassian_blur, 5)
+        img_copy = image.copy()
+        img_copy_2   = image.copy()
 
-    img_copy = image.copy()
-    img_copy_2 = image.copy()
+        _, cnts, _= cv.findContours(image = median, mode = cv.RETR_TREE, method = cv.CHAIN_APPROX_SIMPLE)
 
-    _, cnts, _= cv.findContours(image = median, mode = cv.RETR_TREE, method = cv.CHAIN_APPROX_SIMPLE)
-
-    if cnts:
-        c = max(cnts, key = cv.contourArea)
-        (x,y,w,h) = cv.boundingRect(c)  
-        
-        img_copy_2 = original_image[y:y+h, x:x+w]
-        img_copy[y:y+h, x:x+w] = img_copy_2
+        if cnts:
+            c = max(cnts, key = cv.contourArea)
+            (x,y,w,h) = cv.boundingRect(c)  
+            
+            img_copy_2 = original_image[y:y+h, x:x+w]
+            img_copy[y:y+h, x:x+w] = img_copy_2
 
 
-    cv.imshow("invisible", img_copy)
+        cv.imshow("invisible", img_copy)
 
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
-    frame_num += 1
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
 
-cap.release()
-cv.destroyAllWindows()
+def main():
+    colour_extract()
+    cap = cv.VideoCapture(0)
+    invisible(cap)
+    cap.release()
+    cv.destroyAllWindows()
+
+if __name__=='__main__':
+    main()
